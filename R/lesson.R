833b779fe9a8cc322de7f569ff7920c5c2d994cb
James Clawson <jmclawson@Fitela.local>
2021-02-18 16:47:38 -0600

    - Added Readme explanation for install and use - Added function for easier interface to tutorials

---
+++ R/lesson.R
@@ -0,0 +1,29 @@
+library(dplyr)
+
+lesson <- function() {
+  the.lessons <- c("01-basics")
+
+  print(learnr::available_tutorials("gramda"))
+
+  the.choice <- readline(prompt="Enter the lesson number you'd like to begin with: ")
+
+  bad.entry <- the.choice %>%
+    as.numeric() %>%
+    suppressWarnings() %>%
+    is.na
+
+  if(bad.entry) {
+    message("Please enter only a number.")
+  } else {
+    the.choice <- the.choice %>% as.numeric()
+    if (the.choice == 0) {
+      message("Nice try, but that wasn't one of the options. Please try again.")
+    }
+    else if (the.choice>length(the.lessons)) {
+      message("That lesson isn't yet ready. Please update the gramda package or check back later.")
+    } else {
+      message("The lesson will open in your web browser. When you're finished there and would like to return to functionality in this R Studio window, hit the 'Interrupt R' button in the top-right corner of this console. (The button looks like a stop sign.)")
+      learnr::run_tutorial(the.lessons[the.choice], "gramda")
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# gram_da
+# gramda
+
+Lessons and data for Grambling's DA course
+
+## Installation
+With the devtools package installed, install gramda with the following command:
+
+```{r}
+install_github("jmclawson/gramda")
+```
+
+## Use
+Once installation is complete, the following two lines will get you going with one of the tutorials:
+
+```{r}
+library(gramda)
+lesson()
+```
