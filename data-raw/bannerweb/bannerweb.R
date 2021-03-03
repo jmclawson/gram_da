f70f0b3ff6528fef6f665bf808e6179d215da2b1
James Clawson <clawson@gmail.com>
2021-03-03 14:33:34 -0600

    Added bannerweb dataset, tutotial 2

--- data-raw/bannerweb/bannerweb.R
+++ data-raw/bannerweb/bannerweb.R
@@ -22,13 +22,46 @@ bannerweb <- read.csv("~/Documents/Teaching/Grambling SU/CS 112 Big Data/bannerw
          Start.Time = Time %>%
            strsplit("-") %>%
            sapply(`[`, 1),
-         End.Time = Time %>%
+         `End Time` = Time %>%
            strsplit("-") %>%
-           sapply(`[`, 2)) %>%
+           sapply(`[`, 2)#,
+         # Start.Numeric = Start.Time %>%
+         #   strsplit(":") %>%
+         #   sapply(`[`,1)
+         ) %>%
+  # mutate()
+  mutate(Start.Numeric = case_when(
+    Start.Time=="TBA" ~ as.numeric(NA),
+    TRUE              ~ Start.Time %>%
+      strsplit(":") %>%
+      sapply(`[`,1) %>%
+      as.numeric() %>%
+      suppressWarnings())) %>%
+  mutate(Start.Numeric = case_when(
+    Start.Time=="TBA" ~ as.numeric(NA),
+    grepl("pm", Start.Time)  ~ Start.Numeric + 12,
+    TRUE ~ Start.Numeric
+  )) %>%
+  mutate(Start.Numeric = case_when(
+    Start.Time=="TBA" ~ as.integer(NA),
+    Start.Numeric==24  ~ 12 %>% as.integer(),
+    TRUE ~ Start.Numeric %>% as.integer()
+  )) %>%
+  # mutate(Start.Numeric = case_when(
+  #   Start.Time=="TBA" ~ as.numeric(NA),
+  #   grepl(":30", Start.Time)  ~ Start.Numeric + 0.5,
+  #   TRUE ~ Start.Numeric
+  # )) %>%
+  rename(`Starting Hour` = Start.Numeric,
+         `Start Time` = Start.Time) %>%
+  # mutate(Start.Numeric = case_when(
+  #   is.na(Start.Numeric)           ~ Start.Numeric==NA,
+  #   grepl("30", Start.Time)        ~ Start.Numeric + 0.5
+  # )) %>%
   select(-c(Select, Rem, Location, Time)) %>%
-  relocate(c(Building, Room), .before=Days)
+  relocate(c(Building, Room, Capacity, Enrollment), .before=Days)
 
 bannerweb[bannerweb=="TBA"] <- NA
 bannerweb[bannerweb==""] <- NA
 
-save(file="../data/bannerweb.rda")
+save(bannerweb,file="data/bannerweb.rda")
